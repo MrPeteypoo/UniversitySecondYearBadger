aff0b48e059beeffb2271ec8bff54cb77a46fc0e
Simon Peter Campbell <storm20200@gmail.com>
2015-01-22 00:42:46 +0000

    Implemented the Waypoint class, also removed stdafx.cpp and made main.cpp compile the header.

---
+++ Project/OgreTutorialFramework_2014/src/Path/Path.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#ifndef _PATH_
+#define _PATH_
+
+
+// STL headers.
+#include <memory>
+
+
+// Engine headers.
+//#include <Path/Segment.h>
+
+
+class Path final
+{
+
+private:
+
+    #pragma region Implementation data
+
+    // Forward declarations
+    class Segment;
+    class Waypoint;
+
+    #pragma endregion
+
+};
+
+#endif // _PATH_
--- Project/OgreTutorialFramework_2014/src/stdafx.cpp
+++
@@ -1,1 +0,0 @@
-// Pre-compiled header creation.
